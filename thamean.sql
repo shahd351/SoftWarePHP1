d46016e1f659f037334ac30f52353350b15b246a
lina <445200468@student.ksu.edu.sa>
2026-04-27 21:17:48 +0300

    e
    q

--- thamean.sql
+++ thamean.sql
@@ -105,7 +105,7 @@ CREATE TABLE `request` (
   `CreationDate` datetime NOT NULL,
   `UserID` int(11) DEFAULT NULL,
   `DriverID` int(11) DEFAULT NULL
-  is_edited tinyint(1) NOT NULL DEFAULT '0'
+  'is_edited' tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
